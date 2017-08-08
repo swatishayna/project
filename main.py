201ab16d213b467d9b1a890584d6cf7d734fd463
swatishayna <swati308mmu@gmail.com>
2017-08-08 10:44:13 +0530

    fourth day work

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ spy_salutation = raw_input("What should we call you Mr. or Ms?")
 spy_name = spy_salutation + " " + spy_name   # CONCATENATION OF SALUTATION AND NAME.
 # to check whether spy has input some name or not
 if len(spy_name)>0:
-
  print 'Welcome '  + spy_name + '. Glad to have you back with us.'
+
 
 
